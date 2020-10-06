380c650c6a60825ebf1730ae56d07bbc1c0605a8
Akira Hojo <akira.h.good@gmail.com>
2020-10-06 10:24:52 +0900

    1.0

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
-
-const app = express();
-app.use(index);
-
-const server = http.createServer(app);
-
-const io = socketIo(server); // < Interesting!
-
-let interval;
-
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
+"use strict";
+exports.__esModule = true;
+var crypto_1 = require("crypto");
+var express = require("express");
+var http = require("http");
+var socketIo = require("socket.io");
+// import * as index from "./routes/index";
+var port = process.env.PORT || 4001;
+var app = express();
+// app.use(index);
+var server = http.createServer(app);
+var io = socketIo(server);
+io.on("connection", function (socket) {
+    var interval;
+    console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(function () { return getApiAndEmit(socket); }, 1000);
+    socket.on("disconnect", function () {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    });
 });
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
+var getApiAndEmit = function (socket) {
+    var response = crypto_1.randomInt(100).toString();
+    // Emitting a new message. Will be consumed by the client
+    socket.emit("FromAPI", response);
 };
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, function () { return console.log("Listening on port " + port); });
--- app.ts
+++ app.ts
@@ -3,11 +3,11 @@ import * as express from "express";
 import * as http from "http";
 import * as socketIo from "socket.io";
 
-import * as index from "./routes/index";
+// import * as index from "./routes/index";
 
 const port = process.env.PORT || 4001;
 const app: express.Express = express();
-app.use(index);
+// app.use(index);
 
 const server: http.Server = http.createServer(app);
 const io: socketIo.Server = socketIo(server);
