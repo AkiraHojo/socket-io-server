ce7af52b14e8fa4cf9d30b0f6c76e0f48b0e1987
Akira Hojo <akira.h.good@gmail.com>
2020-10-07 09:52:28 +0900

    Final

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + Socket.IO + Recharts example
+
+I created these codes for my better understanding the React , Socket.IO.
+To use this project , you also need to use the socket-io-client in my repository.
--- app.ts
+++ app.ts
@@ -3,11 +3,8 @@ import * as express from "express";
 import * as http from "http";
 import * as socketIo from "socket.io";
 
-// import * as index from "./routes/index";
-
 const port = process.env.PORT || 4001;
 const app: express.Express = express();
-// app.use(index);
 
 const server: http.Server = http.createServer(app);
 const io: socketIo.Server = socketIo(server);
@@ -27,7 +24,6 @@ io.on("connection", (socket: socketIo.Socket) => {
 
 const getApiAndEmit = (socket: socketIo.Socket) => {
   const response: string = randomInt(100).toString();
-  // Emitting a new message. Will be consumed by the client
   socket.emit("FromAPI", response);
 };
 
